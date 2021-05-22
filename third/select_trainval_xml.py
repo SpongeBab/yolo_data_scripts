49da057364cc6926af886d7bf664cf7f45d222ef
SpongeBab <2078825250@qq.com>
2021-05-22 16:23:20 +0800

    third step

---
+++ third/select_trainval_xml.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+import inspect
+
+
+def select_person(annotation, images, output_annotations):
+    # 第三步，根据已划分好的训练集和验证集图片路径，从VOC文件结构的Annotation中找到对应的xml文件划分
+    if not os.path.exists(output_annotations):
+        os.makedirs(output_annotations)
+    for jpg_file in os.listdir(images):
+        # print(txt_file)
+        jpg_name, extension = os.path.splitext(jpg_file)
+        # print(txt_name)
+        src = os.path.join(annotation, jpg_name + ".xml")
+        dst = os.path.join(output_annotations, jpg_name + ".xml")
+        shutil.copy(src, dst)
+        print("已完成", dst)
+
+
+if __name__ == '__main__':
+    Annotation = "E:\\desk_VOC\\Annotations"
+
+    train_source_images = "E:\\desk_VOC\\images\\train"
+    train_output_annotation = "E:\\desk_VOC\\annotation\\train"
+    select_person(Annotation, train_source_images, train_output_annotation)
+
+    valid_source_images = "E:\\desk_VOC\\images\\valid"
+    valid_output_annotation = "E:\\desk_VOC\\annotation\\valid"
+    select_person(Annotation, valid_source_images, valid_output_annotation)
