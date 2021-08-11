571be933c95ec654e053a310a1691d64064576ac
SpongeBab <2078825250@qq.com>
2021-08-11 16:23:38 +0800

    * 根据xml文件在对应图片上画框 * 验证xml坐标是否生成正确

--- scripts_linux/xml2Bbox.py
+++ scripts_linux/xml2Bbox.py
@@ -29,26 +29,27 @@ def xmin_ymin(xml_path):
 
 # only can draw one picture
 def draw():
-    image = cv2.imread('//coco/VOC/VOC2012/JPEGImages/02_20201017135000_2.jpg')
-    tree = ET.parse('//coco/VOC/VOC2012/Annotations/02_20201017135000_2.xml')
+    image = cv2.imread('E:\\VOC\\face\\images\\train\\0028_80701002007_20191020135000_5.jpg')
+    # image = cv2.resize(image, (640, 360))
+    print(image.shape)
+    tree = ET.parse('E:\\VOC\\face\\annotation\\train\\0028_80701002007_20191020135000_5.xml')
     root = tree.getroot()
-
     for obj in root.iter('object'):
         # Element.find() 找带有特定标签的 第一个 子级，然后可以用 Element.text 访问元素的文本内容。
         Bbox = obj.find('bndbox')
-        xmin = int(Bbox.find("xmin").text)
-        xmax = int(Bbox.find('xmax').text)
-        ymin = int(Bbox.find('ymin').text)
-        ymax = int(Bbox.find('ymax').text)
+        xmin = int(float(Bbox.find("xmin").text))
+        xmax = int(float(Bbox.find('xmax').text))
+        ymin = int(float(Bbox.find('ymin').text))
+        ymax = int(float(Bbox.find('ymax').text))
 
         left = (xmin, ymin)
         right = (xmax, ymax)
         print(left, right)
-        image = cv2.rectangle(image, left, right, (0, 0, 255), 4)
+        image = cv2.rectangle(image, left, right, (0, 0, 255), 3)
         cv2.imshow("1", image)
         cv2.waitKey(0)
 
-    cv2.imwrite("02_20201017135000_2.jpg", image)
+    cv2.imwrite("result.jpg", image)
 
 
 if __name__ == '__main__':
